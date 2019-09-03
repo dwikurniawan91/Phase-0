30a5c20338f54385f78282c711463dd1f3705f06
dwikurniawan91 <dui.dict@gmail.com>
2019-09-03 15:20:45 +0700

    Exercise-6-palindrome-angka

---
+++ Exercise-6-palindrome-angka.js
@@ -0,0 +1,16 @@
+function palindrome(num) {
+    if(!num) return 0;
+    num++;
+    while (true) {
+        let  cekPalindrome = num === Number(num.toString().split('').reverse().join(''));
+        if(!cekPalindrome){
+            num++;
+        }else{
+            return num;
+        }
+    }
+}
+
+console.log(palindrome(82));
+console.log(palindrome(81002));
+console.log(palindrome(10298));
