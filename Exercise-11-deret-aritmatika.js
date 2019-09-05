28b715d9fecf489c7ac8efdb7983c563f3d8b8db
dwikurniawan91 <dui.dict@gmail.com>
2019-09-05 20:23:45 +0700

    Add

--- Exercise-11-deret-aritmatika.js
+++ Exercise-11-deret-aritmatika.js
@@ -1,8 +1,8 @@
 function tentukanDeretAritmatika(arr) {
-    var selisihAngka = arr[1] / arr[0];
+    var selisihAngka = arr[1] - arr[0];
     isDeretAritmatika = true;
     for (let i = 0; i < arr.length-1; i++) {
-        var deretAritmatika = arr[i + 1] / arr[i];
+        var deretAritmatika = arr[i + 1] - arr[i];
         if(deretAritmatika != selisihAngka){
             isDeretAritmatika = false;
         }
@@ -10,9 +10,9 @@ function tentukanDeretAritmatika(arr) {
     return isDeretAritmatika;
 }
   
-  // TEST CASES
-  console.log(tentukanDeretAritmatika([1, 3, 9, 27, 81])); // true
-  console.log(tentukanDeretAritmatika([2, 4, 8, 16, 32])); // true
-  console.log(tentukanDeretAritmatika([2, 4, 6, 8])); // false
-  console.log(tentukanDeretAritmatika([2, 6, 18, 54])); // true
-  console.log(tentukanDeretAritmatika([1, 2, 3, 4, 7, 9])); // false
+ // TEST CASES
+console.log(tentukanDeretAritmatika([1, 2, 3, 4, 5, 6])); // true
+console.log(tentukanDeretAritmatika([2, 4, 6, 12, 24])); // false
+console.log(tentukanDeretAritmatika([2, 4, 6, 8])); // true
+console.log(tentukanDeretAritmatika([2, 6, 18, 54])); // false
+console.log(tentukanDeretAritmatika([1, 2, 3, 4, 7, 9])); // false
